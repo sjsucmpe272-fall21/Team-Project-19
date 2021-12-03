7145857a19df06a12127110c0892acd6855e7c02
Tommy Giang <tommy.giang@sjsu.edu>
2021-12-02 23:20:58 -0800

    Add Period options and Change branch to Course in UI
    Add Period options (ranges from 1 to 20) to represent the lecture number.
    
    Change "branch" to "course" in UI (attendance taken and search attendance)

--- attendence_sys/migrations/0002_attendence_period.py
+++ attendence_sys/migrations/0002_attendence_period.py
@@ -13,6 +13,8 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='attendence',
             name='period',
-            field=models.CharField(choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10')], max_length=200, null=True),
+            field=models.CharField(choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7'), ('8', '8'), ('9', '9'), ('10', '10'),
+                                            ('11', '11'), ('12', '12'), ('13', '13'), ('14', '14'), ('15', '15'), ('16', '16'), ('17', '17'), ('18', '18'), ('19', '19'), ('20', '20')
+                                            ], max_length=200, null=True),
         ),
     ]
